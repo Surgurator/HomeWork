81227cb36681e702efa821955940931458febb20
Surgurator <a9191084222@gmail.com>
2022-11-10 02:26:55 +0300

    Initial Commit

---
+++ Hello Wolrld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello Wolrld
+//
+//  Created by Alexey on 09.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
